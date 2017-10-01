8c7ac7fa4bf24bd47d1f4514da2733ece066071d
Hybridmax <hybridmax95@gmail.com>
2017-10-01 02:50:00 +0200

    sec_battery.c: STORE_MODE: create sysfs to control MIN & MAX battery …
    …capacity charge levels
    
    Change-Id: I4032d8c429a41d7452a902f881d6779c866ca04d

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -30,10 +30,13 @@
 static int wl_polling = 10;
 module_param(wl_polling, int, 0644);
 
-static int STORE_MODE_CHARGING_MAX = 85;
-static int STORE_MODE_CHARGING_MIN = 50;
-module_param_named(store_mode_max, STORE_MODE_CHARGING_MAX, int, 0644);
-module_param_named(store_mode_min, STORE_MODE_CHARGING_MIN, int, 0644);
+static unsigned int STORE_MODE_CHARGING_MAX = 80;
+static unsigned int STORE_MODE_CHARGING_MIN = 25;
+
+module_param_named(store_mode_max, STORE_MODE_CHARGING_MAX, uint, S_IWUSR | S_IRUGO);
+module_param_named(store_mode_min, STORE_MODE_CHARGING_MIN, uint, S_IWUSR | S_IRUGO);
+
+const char *charger_chip_name;
 
 bool sleep_mode = false;
 bool batt_boot_complete = false;
