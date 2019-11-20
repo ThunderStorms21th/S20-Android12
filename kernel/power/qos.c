efc3eb099eb6bcbc8dff0df0b71a5f955747f3e7
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2019-11-20 10:33:34 +0100

    PM: QoS: Invalidate frequency QoS requests after removal
    Switching cpufreq drivers (or switching operation modes of the
    intel_pstate driver from "active" to "passive" and vice versa)
    does not work on some x86 systems with ACPI after commit
    3000ce3c52f8 ("cpufreq: Use per-policy frequency QoS"), because
    the ACPI _PPC and thermal code uses the same frequency QoS request
    object for a given CPU every time a cpufreq driver is registered
    and freq_qos_remove_request() does not invalidate the request after
    removing it from its QoS list, so freq_qos_add_request() complains
    and fails when that request is passed to it again.
    
    Fix the issue by modifying freq_qos_remove_request() to clear the qos
    and type fields of the frequency request pointed to by its argument
    after removing it from its QoS list so as to invalidate it.
    
    Fixes: 3000ce3c52f8 ("cpufreq: Use per-policy frequency QoS")
    Reported-and-tested-by: Doug Smythies <dsmythies@telus.net>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -1393,6 +1393,8 @@ EXPORT_SYMBOL_GPL(freq_qos_update_request);
  */
 int freq_qos_remove_request(struct freq_qos_request *req)
 {
+	int ret;
+
 	if (!req)
 		return -EINVAL;
 
@@ -1400,7 +1402,11 @@ int freq_qos_remove_request(struct freq_qos_request *req)
 		 "%s() called for unknown object\n", __func__))
 		return -EINVAL;
 
-	return freq_qos_apply(req, PM_QOS_REMOVE_REQ, PM_QOS_DEFAULT_VALUE);
+	ret = freq_qos_apply(req, PM_QOS_REMOVE_REQ, PM_QOS_DEFAULT_VALUE);
+	req->qos = NULL;
+	req->type = 0;
+
+	return ret;
 }
 EXPORT_SYMBOL_GPL(freq_qos_remove_request);
 
