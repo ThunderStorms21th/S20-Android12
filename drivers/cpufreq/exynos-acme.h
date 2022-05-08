7c554bcf6448bb6c3df0af6e96608b9755e1cc65
ThunderStorms21th <pinakastorm@gmail.com>
2022-05-08 16:51:49 +0200

    drivers/cpufreq/exynos-acme.h - set TRANSITION_LATENCY at 4000000
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- drivers/cpufreq/exynos-acme.h
+++ drivers/cpufreq/exynos-acme.h
@@ -108,9 +108,9 @@ extern struct list_head *get_domain_list(void);
 
 /*
  * the time it takes on this CPU to switch between
- * two frequencies in nanoseconds
+ * two frequencies in nanoseconds (default: 5000000)
  */
-#define TRANSITION_LATENCY	5000000
+#define TRANSITION_LATENCY	4000000
 
 /*
  * Exynos CPUFreq API
