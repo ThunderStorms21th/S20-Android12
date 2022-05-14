97455625aa9057fecf0ae28b9f91427ebb6dd1d7
ThunderStorms21th <pinakastorm@gmail.com>
2022-05-14 11:38:23 +0200

    Revert "drivers: thermal: Import Xiaomi changes"
    Random reboots !
    
    This reverts commit b657f2c8e758dff7eff878df4e382517b045691b.

--- include/linux/cpu_cooling.h
+++ include/linux/cpu_cooling.h
@@ -84,9 +84,6 @@ struct thermal_cooling_device *
 cpufreq_power_cooling_register(struct cpufreq_policy *policy,
 			       u32 capacitance, get_static_t plat_static_func);
 
-
-void cpu_limits_set_level(unsigned int cpu, unsigned int max_freq);
-
 /**
  * of_cpufreq_cooling_register - create cpufreq cooling device based on DT.
  * @np: a valid struct device_node to the cooling device device tree node.
