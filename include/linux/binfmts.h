25feb7cbece4d9299ccdd66f11f549d3943d5460
ThunderStorms21th <pinakastorm@gmail.com>
2022-07-25 09:25:55 +0200

    Revert "init: add perfservice"
    This reverts commit f8e42b8ec71585c7f472efd0ce701dc81ab67955.

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -162,8 +162,7 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "power@1.3-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "perf@2.0-servic") ||
-	       !strcmp(comm, "init.qcom.post_") ||
-	       !strcmp(comm, "vendor.perfserv");
+	       !strcmp(comm, "init.qcom.post_");
 }
 
 #endif /* _LINUX_BINFMTS_H */
